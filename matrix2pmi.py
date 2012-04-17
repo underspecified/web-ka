d2779daa9b175a763be9b3088287b11ed6fa5df5
underspecified <underspecified@gmail.com>
2012-04-18 01:14:17 +0900

    matrix2pmi.py: many updates
    * implemented discounted PMI from (Pantel&Ravichandran, 2004)
    * generalized to arbitrary number of arguments
    * index pmi collections
    * replaced --update with --start

--- matrix2pmi.py
+++ matrix2pmi.py
@@ -8,10 +8,10 @@ a co-occurence matrix
 
 Creates 4 mongodb collections:
 
-1. <matrix>_f_p
-2. <matrix>_f_i
-3. <matrix>_f_pi
-4. <matrix>_pmi
+1. <matrix>_F_i: instance tuple frequencies
+2. <matrix>_F_p: relation pattern frequencies
+3. <matrix>_F_ip: instance*pattern co-occurence frequencies
+4. <matrix>_pmi_ip: instance*pattern Pointwise Mutual Information score
 
 '''
 
@@ -21,18 +21,18 @@ from bson.code import Code
 from bson.son import SON
 from math import log
 
+from instances2matrix import ensure_indices
 
 class PMI:
-    def __init__(self, db, matrix, update=False):
+    def __init__(self, db, matrix):
         self.db = db
         self.matrix = matrix
-        self.update = update
         self.argv = self.get_args()
         self.argc = len(self.argv)
-        self._F_i = self.make_F_i()
-        self._F_p = self.make_F_p()
-        self._F_ip = self.make_F_ip()
-        self._pmi_ip = self.make_pmi_ip()
+        self._F_i = '%s_F_i' % self.matrix
+        self._F_p = '%s_F_p' % self.matrix
+        self._F_ip = '%s_F_ip' % self.matrix
+        self._pmi_ip = '%s_pmi_ip' % self.matrix
 
     def get_args(self):
         '''returns a lists of argument names in <matrix>'''
@@ -58,13 +58,10 @@ class PMI:
                        '  );'
                        '  return {score:sum};'
                        '}')
-        F_i = '%s_F_i' % self.matrix
-        if self.update:
-            self.db[self.matrix].map_reduce(
-                map_, reduce_, F_i, full_response=True
-                )
+        self.db[self.matrix].map_reduce(
+            map_, reduce_, self._F_i, full_response=True
+            )
         print >>sys.stderr, 'making instance counts: done.'
-        return F_i
 
     def make_F_p(self):
         '''creates a collection <matrix>_F_p containing relation pattern
@@ -80,24 +77,21 @@ class PMI:
                        '  );'
                        '  return {score:sum};'
                        '}')
-        F_p = '%s_F_p' % self.matrix
-        if self.update:
-            self.db[self.matrix].map_reduce(
-                map_, reduce_, F_p, full_response=True
-                )
+        self.db[self.matrix].map_reduce(
+            map_, reduce_, self._F_p, full_response=True
+            )
         print >>sys.stderr, 'making pattern counts: done.'
-        return F_p
 
     def make_F_ip(self):
-        '''creates a collection <matrix>_F_p containing instance*pattern
+        '''creates a collection <matrix>_F_ip containing instance*pattern
         frequencinces and returns its name'''
         print >>sys.stderr, 'making instance*pattern counts...'
         arg_str = ['%s:this.%s'%(a,a) 
                    for a in self.argv]
         map_ = Code('function () {'
                     '  var d = {};'
-                    '  for (i=1;i<=n;i++) {d["arg"+i] = this["arg"+i]}'
                     '  d["rel"] = this.rel;'
+                    '  for (i=1;i<=n;i++) {d["arg"+i] = this["arg"+i]}'
                     '  emit(d, {score:this.score});'
                     '}', n=self.argc)
         reduce_ = Code('function (key, values) {'
@@ -107,36 +101,40 @@ class PMI:
                        '  );'
                        '  return {score:sum};'
                        '}')
-        F_ip = '%s_F_ip' % self.matrix
-        if self.update:
-            self.db[self.matrix].map_reduce(
-                map_, reduce_, F_ip, full_response=True
-                )
+        self.db[self.matrix].map_reduce(
+            map_, reduce_, self._F_ip, full_response=True
+            )
         print >>sys.stderr, 'making instance*pattern counts: done.'
-        return F_ip
 
     def make_pmi_ip(self):
         '''creates a collection <matrix>_pmi_ip containing instance*relation
         Pointwise Mutual Information scores and returns its name'''
         print >>sys.stderr, 'calculating instance*pattern PMI...'
-        pmi_ip = '%s_pmi_ip' % self.matrix
-        for x in self.db[self.matrix].find():
-            i = [x[a] for a in self.argv]
-            p = x['rel']
-            pmi = self.pmi(i,p)
-            x['pmi'] = pmi
-            x.pop('_id')
-            self.db[pmi_ip].save(x)
+        with self.db[self.matrix].find() as xs:
+            for n,x in enumerate(xs, 1):
+                p = x['rel']
+                rel = [('rel', p), ]
+                i = [x[a] for a in self.argv]
+                args = zip(self.argv, i)
+                pmi = zip(('dpmi', 'discount', 'pmi'), self.discounted_pmi(i,p))
+                y = SON(rel+args+pmi)
+                self.db[self._pmi_ip].save(y)
+                if n%10000 == 0:
+                    print >>sys.stderr, '# %8d PMI scores calculated' % n
         print >>sys.stderr, 'calculating instance*pattern PMI: done.'
-        return pmi_ip
+        ensure_indices(self.db, self._pmi_ip)
 
     def I(self):
         '''returns all instance tuples in <matrix>'''
-        return self.db[self.matrix].find(fields=self.argv)
+        with self.db[self.matrix].find(fields=self.argv, timeout=False) as xs:
+            for x in xs:
+                yield x
 
     def P(self):
         '''returns all relation patterns in <matrix>'''
-        return self.db[self.matrix].find(fields=['rel'])
+        with self.db[self.matrix].find(fields=['rel'], timeout=False):
+            for x in xs:
+                yield x
 
     def i2query(self, i):
         '''returns a tuple of argument values labeled (arg1,<value>),
@@ -149,6 +147,7 @@ class PMI:
         try:
             query = SON({'_id':self.i2query(i)})
             #print >>sys.stderr, 'query:', query
+            #print >>sys.stderr, self.db[self._F_i].find(query).explain()
             v = self.db[self._F_i].find_one(query, fields=['value'])
             #print >>sys.stderr, 'v:', v
             return v['value']['score']
@@ -162,30 +161,36 @@ class PMI:
         try:
             query = SON({'_id':{'rel':p}})
             #print >>sys.stderr, 'query:', query
+            #print >>sys.stderr, self.db[self._F_p].find(query).explain()
             v = self.db[self._F_p].find_one(query, fields=['value'])
             #print >>sys.stderr, 'v:', v
             return v['value']['score']
         except Exception as e:
-            print >>sys.stderr, e, i
+            print >>sys.stderr, e, p
             raise e
 
     def F_ip(self, i, p):
         '''calculate the co-occurence frequency (i.e. the sum of scores) of 
         instance*pattern'''        
         try:
-            query = self.i2query(i)
-            query.update({'rel':p})
+            query = SON({'rel':p})
+            query.update(self.i2query(i))
             #print >>sys.stderr, 'query:', query
+            #print >>sys.stderr, self.db[self._F_ip].find(query).explain()
             v = self.db[self._F_ip].find_one({'_id':query}, fields=['value'])
             #print >>sys.stderr, 'v:', v
             return v['value']['score']
         except Exception as e:
-            print >>sys.stderr, e, i
+            print >>sys.stderr, e, i, p
             raise e
 
     def pmi(self, i, p):
         '''pmi: pointwise mutual information between instance and pattern'''
-        return log( self.F_ip(i,p) / (self.F_p(p)*self.F_i(i)) )
+        return self._pmi(self.F_i(i), self.F_p(p), self.F_ip(i,p))
+
+    def _pmi(self, F_i, F_p, F_ip):
+        '''pmi: pointwise mutual information between instance and pattern'''
+        return log( F_ip / (F_p*F_i) )
 
     def max_pmi(self):
         '''max_pmi: maximum PMI between instance and pattern'''
@@ -194,16 +199,51 @@ class PMI:
     def r_p(self, p):
         '''r_p: reliability of pattern p'''
         return sum( [self.pmi(i,p)*self.r_i(i) /
-                     self._max_pmi for i in self._I] ) / len(I)
+                     self._max_pmi for i in self._I] ) / len(self._I)
 
     def r_i(self, i):
         '''r_i: reliability of instance i'''
-        return sum( [self.pmi(i,p)*self.r_p(p) / 
-                     self._max_pmi for p in self._P] ) / len(P)
+        return sum( [self.pmi(i,p)*self.r_p(p) /
+                     self._max_pmi for p in self._P] ) / len(self._P)
 
-# def discount():
-#     '''discounting factor for PMI towards infrequent elements'''
-#     return (C(e,f) / (C(e,f)+1)) * (min( sum([C(e,i) for i in xrange(1,n)]), sum([C(j,f) for i in xrange(1,m)]) ) / (min( sum([C(e,i) for i in xrange(1,n)]), sum([C(j,f) for i in xrange(1,m)]) ) + 1)
+    def smooth(self, x, y=1.0):
+        '''returns value of x smoothed by y'''
+        return x / (x+y)
+
+    def discount(self, i, p):
+        '''discounting factor for PMI towards infrequent elements
+        See Equation (2) from:
+        Patrick Pantel and Deepak Ravichandran.
+        Automatically Labeling Semantic Classes.
+        HLT-NAACL 2004.'''
+        return self._discount( c_ef=self.F_ip(i,p), c_ei=self.F_i(i), c_jf=self.F_p(p) )
+
+    def _discount(self, c_ef, c_ei, c_jf):
+        '''discounting factor for PMI towards infrequent elements
+        See Equation (2) from:
+        Patrick Pantel and Deepak Ravichandran.
+        Automatically Labeling Semantic Classes.
+        HLT-NAACL 2004.'''
+        return self.smooth(c_ef,1.0) * self.smooth(min(c_ei,c_jf),1.0)
+
+    def discounted_pmi(self, i, p):
+        '''returns a tuple of (discount*pmi, discount, pmi)'''
+        F_i = self.F_i(i)
+        F_p = self.F_p(p)
+        F_ip = self.F_ip(i,p) 
+        pmi = self._pmi(F_i, F_p, F_ip)
+        discount = self._discount(c_ef=F_ip, c_ei=F_i, c_jf=F_p)
+        dpmi = pmi*discount
+        return dpmi, discount, pmi
+
+def validate_start(s):
+    '''maps starting collection name to order number returning 0 if invalid request'''
+    fs = ('F_i', 'F_p', 'F_pi', 'pmi_ip')
+    ns = ('1', '2', '3', '4')
+    d = {}
+    d.update(zip(fs, xrange(1,5)))
+    d.update(zip(ns, xrange(1,5)))
+    return d.get(s,0)
 
 def main():
     from optparse import OptionParser
@@ -213,16 +253,34 @@ def main():
                       help='''mongodb host machine name. default: localhost''')    
     parser.add_option('-p', '--port', dest='port', type=int, default=1979,
                       help='''mongodb host machine port number. default: 1979''')
-    parser.add_option('-u', '--update', dest='update',
-                      action="store_true", default=False,
-                      help='''update co-occurence counts. default: False''')
+    parser.add_option('-s', '--start', dest='start', default='F_i',
+                      help='''specify calculation to start with
+                              1 or F_i: instance tuple frequencies
+                              2 or F_p: relation pattern frequencies
+                              3 or F_ip: instance*pattern co-occurence frequencies
+                              4 or pmi_ip: instance*pattern Pointwise Mutual Information score
+                              default: F_i''')
     options, args = parser.parse_args()
     if len(args) != 2:
         parser.print_help()
         exit(1)
+    start = validate_start(options.start)
+    if start == 0:
+        print >>sys.stderr, 'start option is invalid! %s' % option.start
+        parser.print_help()
+        exit(1)
+
     db, collection = args
     connection = pymongo.Connection(options.host, options.port)
-    PMI(connection[db], collection, options.update)
+    p = PMI(connection[db], collection)
+    if start <= 1:
+        p.make_F_i()
+    if start <= 2:
+        p.make_F_p()
+    if start <= 3:
+        p.make_F_ip()
+    if start <= 4:
+        p.make_pmi_ip()
 
 if __name__ == '__main__':
     main()
