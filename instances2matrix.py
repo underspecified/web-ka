d30ac05f864a8688a2d5800aafc14e6996703630
underspecified <underspecified@gmail.com>
2012-04-18 16:09:03 +0900

    updated docstrings

--- instances2matrix.py
+++ instances2matrix.py
@@ -4,8 +4,22 @@
 ################################################################################
 
 '''
-instances2matrix.py: creates a matrix of co-occurence counts between relation 
-pattern x arguments in mongodb from input instances
+`instances2matrix.py`: creates a matrix of co-occurence counts between
+relation pattern * arguments in mongodb from input instances
+
+### Usage
+
+	Usage: instances2matrix.py [options] [<instance_files>]
+
+	Options:
+  	-h, --help            show this help message and exit
+  	-c COLLECTION, --collection=COLLECTION  
+  		              collection name
+  	-d DB, --database=DB  database name
+  	-o HOST, --host=HOST  mongodb host machine name. 
+  			      default: localhost
+  	-p PORT, --port=PORT  mongodb host machine port number. 
+  			      default: 27017
 
 ### Instances
 
@@ -13,34 +27,34 @@ pattern x arguments in mongodb from input instances
 
 Instances have the following tab-delimited format:
 
-* score: score representing weight * co-occurence count for instance
-* loc: giving source and location of instance
-* rel: containing relation pattern
-* argc: giving argument count
-* argv: tab-delimited list of arguments as strings
+* `score`: score representing weight * co-occurence count for instance
+* `loc`: giving source and location of instance
+* `rel`: containing relation pattern
+* `argc`: giving argument count
+* `argv`: tab-delimited list of arguments as strings
 
 #### Example
 
-    1.0\treverb_clueweb_tuples-1.1.txt:30:10-11\tARG1 acquired ARG2\t2\tGoogle\tYouTube
+    1.0\treverb_clueweb_tuples-1.1.txt:30:10-11\tARG1 acquired ARG2\t2\Google\tYouTube
      
 ### Co-occurence Matrix
 
 #### Format
    
 The co-occurence matrix collection has the following fields:
-
-* rel: relation pattern
-* arg1: first argument
+     
+* `rel`: relation pattern
+* `arg1`: first argument
 * ...
-* argn: nth argument
-* score: score for rel x args tuple
+* `argn`: nth argument
+* `score`: score for rel * args tuple
 
 #### Naming Scheme
 
-Instances of differing argument count are stored in separate mongodb collections
-with names formatted as `<collection>_<argc>`. E.g. if a collection `clueweb`
-has instances with argument counts of 1, 2, and 3, then the following collection
-would be created:
+Instances of differing argument count are stored in separate mongodb
+collections with names formatted as `<collection>_<argc>`. E.g. if a
+collection `clueweb` has instances with argument counts of 1, 2, and
+3, then the following collection would be created:
  
 * `clueweb_1`
 * `clueweb_2`
